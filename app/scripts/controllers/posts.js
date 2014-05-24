ec85e7f8aa5034d492f07728e8387fc048eeb334
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-24 17:33:11 -0500

    Add delete deletePost method on ng-click,  holds index of current object in data set

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -7,5 +7,9 @@ app.controller('PostsCtrl', function($scope){
   $scope.submitPost = function(){
     $scope.posts.push($scope.post);
     $scope.post = {url: 'http://', title: ''};
-  }
+  };
+
+  $scope.deletePost = function(index){
+    $scope.posts.splice(index, 1);
+  };
 });
