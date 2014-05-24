dbed9c99ace822e8046cf2fbeb9d4db116a58b31
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-24 17:22:55 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var app = angular.module('angNewsApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ]);
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
