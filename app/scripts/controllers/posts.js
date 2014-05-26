6b4140769074ca2c774965fad73c6486f6126c47
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-26 17:43:54 -0500

    Redirect to post page after submitting a post

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,13 +1,14 @@
 'use strict';
 
-app.controller('PostsCtrl', function($scope, Post){
+app.controller('PostsCtrl', function($scope, $location, Post){
   $scope.posts = Post.all;
 
   $scope.post = {url: 'http://', 'title': ''};
 
   $scope.submitPost = function(){
-    Post.create($scope.post).then(function(){
-      $scope.post = {url: 'http://', 'title': ''};
+    Post.create($scope.post).then(function(ref){
+      // $scope.post = {url: 'http://', 'title': ''}; // this only needed if staying on the page, but since we're redirecting to another page, not needed
+      $location.path('/posts' + ref.name());
     });
   };
 
