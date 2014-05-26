41680b9ebae2a67718f641f340dd321346b9a7bd
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-25 19:01:17 -0500

    Update post controller to use Firebase, return promise, chain .then and pass fx that will get called once operation completes

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,20 +1,18 @@
 'use strict';
 
 app.controller('PostsCtrl', function($scope, Post){
-  $scope.posts = [];
-  $scope.post = {url: 'http://', title: ''};
+  $scope.posts = Post.all;
+
+  $scope.post = {url: 'http://', 'title': ''};
 
   $scope.submitPost = function(){
-    Post.save($scope.post, function(ref){
-      $scope.posts[ref.name] = $scope.post;
-      $scope.post = {url: 'http://', title: ''};
+    Post.create($scope.post).then(function(){
+      $scope.post = {url: 'http://', 'title': ''};
     });
   };
 
   $scope.deletePost = function(postId){
-    Post.delete({id: postId}, function(){
-      delete $scope.posts[postId];
-    });
+    Post.delete(postId);
   };
 
 });
