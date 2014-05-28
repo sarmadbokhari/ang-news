9d953c78810624f76ba45e4f756b2e27de00f3e0
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-28 17:33:26 -0500

    TYPO fix, missing curly brace for function

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.factory('Auth', function($firebaseSimpleLogin, FIREBASE_URL, $rootScope)
+app.factory('Auth', function($firebaseSimpleLogin, FIREBASE_URL, $rootScope){
   var ref = new Firebase(FIREBASE_URL);
 
   var auth = $firebaseSimpleLogin(ref);
