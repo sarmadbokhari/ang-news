693788ae94bafcc9b63730d66a3e88cef9db5dd9
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-27 19:21:13 -0500

    Create auth controller to check if user is signed in, if not go to register

---
+++ app/scripts/controllers/auth.js
@@ -0,0 +1,14 @@
+'use strict';
+
+app.controller('AuthCtrl', function($scope, $location, Auth){
+  if (Auth.signedIn()){
+    $location.path('/');
+  }
+
+  $scope.register = function(){
+    Auth.register($scope.user).then(function(authUser){
+      console.log(authUser);
+      $location.path('/');
+    });
+  };
+});
