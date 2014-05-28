960ce2bde160c155fdebc8718d0e5a37d08ea8e4
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-27 19:34:55 -0500

    Add navigation logout controller and view

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('NavCtrl', function($scope, $location, Post){
+app.controller('NavCtrl', function($scope, $location, Post, Auth){
   $scope.post = {url: "http://", title: ''};
 
   $scope.submitPost = function(){
@@ -8,6 +8,10 @@ app.controller('NavCtrl', function($scope, $location, Post){
       // $scope.post = {url: 'http://', 'title': ''}; // this only needed if staying on the page, but since we're redirecting to another page, not needed
       $location.path('/posts' + ref.name());
     });
+  };
+
+  $scope.logout = function(){
+    Auth.logout();
   };
 
 });
