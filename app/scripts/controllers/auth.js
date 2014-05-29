ab7f4d952e6e3f72ac1691614a15f19cebb469c4
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-28 21:47:35 -0500

    Update Auth controller to kick user to root '/' if they are already logged in

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -5,6 +5,16 @@ app.controller('AuthCtrl', function($scope, $location, Auth){
     $location.path('/');
   }
 
+  $scope.$on('$firebaseSimpleLogin:login', function(){
+    $location.path('/');
+  });
+
+  $scope.login = function(){
+    Auth.login($scope.user).then(function(){
+      $location.path('/');
+    });
+  };
+
   $scope.register = function(){
     Auth.register($scope.user).then(function(authUser){
       console.log(authUser);
