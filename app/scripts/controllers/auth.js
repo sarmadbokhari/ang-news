5db915a3f6b58e8236bc7f45f2453be200e5ae04
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-30 23:04:40 -0500

    Add error handling to auth controller

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -12,13 +12,17 @@ app.controller('AuthCtrl', function($scope, $location, Auth){
   $scope.login = function(){
     Auth.login($scope.user).then(function(){
       $location.path('/');
-    })
+    }, function(error){
+      $scope.error = error.toString();
+    });
   };
 
   $scope.register = function(){
     Auth.register($scope.user).then(function(authUser){
       console.log(authUser);
       $location.path('/');
+    }, function(error){
+      $scope.error = error.toString();
     });
   };
 });
