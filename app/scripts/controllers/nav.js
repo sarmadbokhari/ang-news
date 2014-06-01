b80eab42c71095dc80b6e113a5853a99d3ac3bd0
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-31 20:45:54 -0500

    Update NavCtrl to work with new post promise (which returns postId)

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -4,9 +4,9 @@ app.controller('NavCtrl', function($scope, $location, Post, Auth){
   $scope.post = {url: "http://", title: ''};
 
   $scope.submitPost = function(){
-    Post.create($scope.post).then(function(ref){
-      // $scope.post = {url: 'http://', 'title': ''}; // this only needed if staying on the page, but since we're redirecting to another page, not needed
-      $location.path('/posts' + ref.name());
+    Post.create($scope.post).then(function(postId){
+      $scope.post = {url: 'http://', 'title': ''};
+      $location.path('/posts' + postId);
     });
   };
 
