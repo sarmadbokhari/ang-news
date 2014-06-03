3252aa0cdd96d8833447096693dcc12764574818
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-06-02 22:04:44 -0500

    Typo fix

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -5,7 +5,7 @@ app.controller('NavCtrl', function($scope, $location, Post, Auth){
 
   $scope.submitPost = function(){
     Post.create($scope.post).then(function(postId){
-      $scope.post = {url: 'http://', 'title': ''};
+      $scope.post = {url: 'http://', title: ''};
       $location.path('/posts/' + postId);
     });
   };
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -3,7 +3,7 @@
 app.controller('PostsCtrl', function($scope, $location, Post){
   $scope.posts = Post.all;
 
-  $scope.post = {url: 'http://', 'title': ''};
+  $scope.post = {url: 'http://', title: ''};
 
   $scope.deletePost = function(postId){
     Post.delete(postId);
