1b23d1a46981978d587efe31f1bcff1f156bcdb9
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-06-03 16:38:43 -0500

    Add postview controller methods to addComment and removeComment

--- app/scripts/controllers/postview.js
+++ app/scripts/controllers/postview.js
@@ -2,4 +2,14 @@
 
 app.controller('PostViewCtrl', function($scope, $routeParams, Post){
   $scope.post = Post.find($routeParams.postId);
+
+  $scope.addComment = function(){
+    Post.addComment($routeParams.postId, $scope.comment);
+    $scope.comment = '';
+  };
+
+  $scope.removeComment = function(){
+    Post.deleteComment($scope.post, comment, commentId)
+  }
+
 });
