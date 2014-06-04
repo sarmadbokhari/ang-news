29465a6c7ee2afc89c56491da63b92803b59c218
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-06-04 01:47:41 -0500

    Add populateComments method to profile controller

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -12,5 +12,19 @@ app.controller('ProfileCtrl', function($scope, $routeParams, Post, User){
       $scope.posts[postId] = Post.find(postId);
     });
   }
+
+  function populateComments(){
+    $scope.comments = {};
+
+    angular.forEach($scope.user.comments, function(comment){
+      var post = Post.find(comment.postId);
+
+      post.$on('loaded', function(){
+        $scope.comments[comment.id] = post.$child('comments').child(comment.id);
+
+        $scope.commentedPosts[comment.postId] = post;
+      });
+    });
+  }
 });
 
