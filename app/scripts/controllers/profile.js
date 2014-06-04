49aed14a649979d493ee1a43b0e00b4f74274df4
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-06-04 02:12:31 -0500

    Update profile controller to show comments

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -3,7 +3,12 @@
 app.controller('ProfileCtrl', function($scope, $routeParams, Post, User){
   $scope.user = User.findByUserName($routeParams.username);
 
-  $scope.user.$on('loaded', populatePosts);
+  $scope.commentedPosts = {};
+
+  $scope.user.$on('loaded', function(){
+    populatePosts();
+    populateComments();
+  });
 
   function populatePosts(){
     $scope.posts = {};
@@ -20,7 +25,7 @@ app.controller('ProfileCtrl', function($scope, $routeParams, Post, User){
       var post = Post.find(comment.postId);
 
       post.$on('loaded', function(){
-        $scope.comments[comment.id] = post.$child('comments').child(comment.id);
+        $scope.comments[comment.id] = post.$child('comments').$child(comment.id);
 
         $scope.commentedPosts[comment.postId] = post;
       });
