3b329d978e4259122f55822242b8b65bd47b0e20
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-06-04 01:38:50 -0500

    Add populatePosts method to profile controller

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -2,5 +2,15 @@
 
 app.controller('ProfileCtrl', function($scope, $routeParams, Post, User){
   $scope.user = User.findByUserName($routeParams.username);
+
+  $scope.user.$on('loaded', populatePosts);
+
+  function populatePosts(){
+    $scope.posts = {};
+
+    angular.forEach($scope.user.posts, function(postId){
+      $scope.posts[postId] = Post.find(postId);
+    });
+  }
 });
 
